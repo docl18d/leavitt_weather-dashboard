eabb07c621c0466ec7a657d1a420d6613d0cdcf7
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-10-23 15:03:00 -0600

    added the 5 day forcast cards.  Completed the API links, changed temperature to farenheit.

--- styles/script.js
+++ styles/script.js
@@ -97,3 +97,36 @@ $(document).ready(function () {
             $("#today").append(card);
             console.log(data);
 });
+    }
+// Five Day forcast cards
+    function weatherForecast(searchTerm) {
+        $.ajax({
+            type: "GET",
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=9e54fcc6a6103cd924ea373317c87301&units=imperial",
+
+        }).then(function (data) {
+            console.log(data);
+            $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+
+            for (var i = 0; i < data.list.length; i++) {
+
+                if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+
+                    var titleFive = $("<h3>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+                    var imgFive = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+
+                    var colFive = $("<div>").addClass("col-md-2.5");
+                    var cardFive = $("<div>").addClass("card bg-primary text-white");
+                    var cardBodyFive = $("<div>").addClass("card-body p-2");
+                    var humidFive = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
+                    var tempFive = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp + " °F");
+     
+                    colFive.append(cardFive.append(cardBodyFive.append(titleFive, imgFive, tempFive, humidFive)));
+                    $("#forecast .row").append(colFive);
+
+                }
+            }
+        });
+    }
+
+});
