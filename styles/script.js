c18c730703810d814323647376f8dfe9e430f994
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-10-23 13:11:11 -0600

    added local storage and event listener for list

--- styles/script.js
+++ styles/script.js
@@ -1,27 +1,37 @@
-$(function () {
-    
-    // search button function
-    $("search-button").on("click", function () {
-        var searchTerm = $("#search-value").val();
-        $("#search-value").val("");
-        weatherFunction(searchTerm);
-        weatherFunction(searchTerm);
+$(document).ready(function () {
+// search button function
+    $("#search-button").on("click", function () {
+    var searchTerm = $("#search-value").val();
+    $("#search-value").val("");
+    weatherFunction(searchTerm);
+    weatherForecast(searchTerm);
+});
 
-        
-    });
-
-    // keyboard enter button event
-    $("search-button").keypress(function (e) { 
-        var keyCode = (e.keyCode ? e.keycode : e.which);
-        if (keyCode === 13) {
+// keyboard 'enter' button event
+    $("#search-button").keypress(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode === 13) {
             weatherFunction(searchTerm);
-            weatherFunction(searchTerm);
+            weatherForecast(searchTerm);
         }
-    });
+});
    
+    // Search Storage and history row
+    var history = JSON.parse(localStorage.getItem("history")) || [];
 
+    if (history.length > 0) {
+        weatherFunction(history[history.length - 1]);
+    }
+    for (var i = 0; i < history.length; i++) {
+        createRow(history[i]);
+    }
 
-
-
-
-})
+    function createRow(text) {
+        var listItem = $("<li>").addClass("list-group-item").text(text);
+        $(".history").append(listItem);
+    }
+    // list item click functionality listener
+    $(".history").on("click", "li", function () {
+        weatherFunction($(this).text());
+        weatherForecast($(this).text());
+});
